c5b792bed0f69fdc599ea6b7391110212dd3662c
PayneTang <payne.appdeveloper@gmail.com>
2019-11-03 01:07:38 +0800

    Change csv name to be variable in model.py

--- app/model.py
+++ app/model.py
@@ -6,6 +6,10 @@ import json
 import random
 # import os
 
+# Please change the csv file path in below variable
+csv_name = "/Users/tangpayne/Documents/WebApplication/MAMP/projects/webnotebook/storage.csv" #Payne
+# csv_name = "/home/mushcat/webnotebook/storage.csv" # Mushcat
+
 def postSearching(rowList = None, dfLen = None, df = None):
 	if rowList is None:
 		rowList = range(dfLen)
@@ -23,11 +27,11 @@ class kPost:
 		# required to test out resource
 		data = json.dumps(input_)
 		df = pd.read_json(data)
-		df.to_csv("/home/mushcat/webnotebook/storage.csv", mode="a", index=False, header=False)
+		df.to_csv(csv_name, mode="a", index=False, header=False)
 		return "complete"
 
 	def readRow(self, title=None, total=None):
-		df = pd.read_csv("/home/mushcat/webnotebook/storage.csv", dtype=str)
+		df = pd.read_csv(csv_name, dtype=str)
 		# search by title return list of row numbers
 		if title is not None:
 			rowList = df.index[df["title"] == str(title)].tolist()
@@ -45,19 +49,19 @@ class kPost:
 		postId = input_[0]['postId']
 		data = json.dumps(input_)
 		df = pd.read_json(data)
-		storageDf = pd.read_csv('/home/mushcat/webnotebook/storage.csv')
+		storageDf = pd.read_csv(csv_name)
 		targetIndex = storageDf[storageDf['postId'] == int(postId)].index
 		storageDf.drop(targetIndex, inplace=True)
 		storageDf = storageDf.append(df)
 		storageDf.reset_index()
-		storageDf.to_csv("/home/mushcat/webnotebook/storage.csv", index=False)
+		storageDf.to_csv(csv_name, index=False)
 		return 'complete'
 
 	def deleteRow(self, title):
-		df = pd.read_csv('/home/mushcat/webnotebook/storage.csv')
+		df = pd.read_csv(csv_name)
 		targetIndex = df[df['title'] == title].index
 		df.drop(targetIndex, inplace=True)
-		df.to_csv("/home/mushcat/webnotebook/storage.csv", index=False)
+		df.to_csv(csv_name, index=False)
 		return 'complete'
 
 def test_kPost(**kwargs):
@@ -75,7 +79,7 @@ class user:
 		pass
 
 	def display_userPost(self, userId):
-		df = pd.read_csv("/home/mushcat/webnotebook/storage.csv", dtype=str)
+		df = pd.read_csv(csv_name, dtype=str)
 		# search by title return list of row numbers
 		rowList = df.index[df["userId"] == str(userId)].tolist()
 		returnValue = []
@@ -86,22 +90,3 @@ def test_user():
 	result = new.display_userPost(1)
 	print(result)
 
-		
-"""
-{
-	"postId": 1
-	"userId": 1
-	"title": "proton"
-	"content": "The electron is a subatomic particle, symbol e− or β−, whose electric charge is negative one elementary charge"
-	"relatedId": [1]
-	"resource": [{}]
-	# to be designed
-}
-
-
-input_ = [{"postId": "3", "userId": "2", "title": "neutron", "content": "The neutron is a subatomic particle, symbol n or n0, with no net electric charge and a mass slightly greater than that of a proton", "relatedId": "1,2,3", "resource": "[]"}]
-"""
-
-# test_kPost(input_= input_)
-# test_user()
-# print(os.getcwd())
